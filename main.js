6c8000ee4a6fe9c7ac3e40699427f47ebc4a1183
홍왕열 <love88boa@naver.com>
2022-05-06 17:49:27 +0900

    navbar scroll에 따른 변화 적용
    navbar scroll에 따른 변화 적용

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+"use strict";
 
+// 스크롤에 따라 navbar 변화
+
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener("scroll", () => {
+  //   console.log(window.scrollY);
+  //   console.log(navbarHeight);
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+});
