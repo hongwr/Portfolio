a024dbe3dbf15ac7267c98b8d1492b484fd9caae
홍왕열 <love88boa@naver.com>
2022-05-09 17:27:58 +0900

    navbar css js 적용
    navbar css js 적용

--- main.js
+++ main.js
@@ -4,6 +4,7 @@
 
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
+//사이즈 보는 함수9눈에 보여지는 Element size. 크기 옵션이 전부 적용된 size. offsetwidth랑 비교
 
 document.addEventListener("scroll", () => {
   //   console.log(window.scrollY);
@@ -13,4 +14,19 @@ document.addEventListener("scroll", () => {
   } else {
     navbar.classList.remove("navbar--dark");
   }
+});
+
+// Handle scrolling when tapping on the navbar menu
+
+const navbarMenu = document.querySelector(".navbar__menu");
+
+navbarMenu.addEventListener("click", (e) => {
+  const target = e.target;
+  const link = target.dataset.link;
+  if (link === null) {
+    return;
+  }
+  console.log(e.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
 });
