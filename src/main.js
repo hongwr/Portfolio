6118ec0a0a7e4a19ae9ad1237a67d7a09c268c76
hongwr <hongwr88@gmail.com>
2023-08-06 00:05:09 +0900

    update arrow up css and js function

--- src/main.js
+++ src/main.js
@@ -21,3 +21,13 @@ const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
     home.style.opacity = 1 - window.scrollY / homeHeight;
 });
+
+// Arrow up 버튼을 아래로 스크롤 시 투명하게 처리함
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if (window.scrollY > homeHeight / 2) {
+        arrowUp.style.opacity = 1;
+    } else {
+        arrowUp.style.opacity = 0;
+    }
+});
