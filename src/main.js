9e7ca3eab45cae8d33e609e04d2753d8066f51b3
hongwr <hongwr88@gmail.com>
2023-08-06 01:44:39 +0900

    update projects function and css

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ const header = document.querySelector('header');
 const headerHeight = header.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY);
     if (window.scrollY > headerHeight) {
         header.classList.add('header--dark');
     } else {
@@ -30,4 +29,18 @@ document.addEventListener('scroll', () => {
     } else {
         arrowUp.style.opacity = 0;
     }
+});
+
+// Navbar 토글 버튼 클릭 처리
+
+const navbarMenu = document.querySelector('.header__menu');
+const navbarToggle = document.querySelector('.header__toggle');
+navbarToggle.addEventListener('click', () => {
+    // class가 있으면 켜지고 없으면 꺼지는 기능, toggle
+    navbarMenu.classList.toggle('open');
+});
+
+// Navbar 메뉴 클릭 시 메뉴를 자동으로 닫아줌
+navbarMenu.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
 });
