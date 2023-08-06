d704a4e0292ec2a796e404e97229e130e097f3f3
hongwr <hongwr88@gmail.com>
2023-08-06 18:30:25 +0900

    update active menu

---
+++ src/active_menu.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const sectionIds = ['#home', '#about', '#skills', '#work', '#contact'];
+const sections = sectionIds.map((id) => document.querySelector(id));
+const navItems = sectionIds.map((id) => document.querySelector(`[href="${id}"]`));
+const visibleSections = sectionIds.map(() => false);
+
+const options = {
+    rootMargin: '-20% 0px 0px 0px',
+    threshold: [0, 0.98],
+};
+const observer = new IntersectionObserver(observerCallback, options);
+sections.forEach((section) => observer.observe(section));
+
+function observerCallback(entries) {
+    let selectLastOne;
+    entries.forEach((entry) => {
+        const index = sectionIds.indexOf(`#${entry.target.id}`);
+        visibleSections[index] = entry.isIntersecting;
+        selectLastOne = index === sectionIds.length - 1 && entry.isIntersecting && entry.intersectionRatio >= 0.98;
+    });
+    console.log(visibleSections);
+    console.log('무조건 라스트 섹션!!', selectLastOne);
+
+    const navIndex = selectLastOne ? sectionIds.length - 1 : findFirstIntersecting(visibleSections);
+    console.log(sectionIds[navIndex]);
+}
+
+function findFirstIntersecting(intersections) {
+    const index = intersections.indexOf(true);
+    console.log('첫번째 인덱스', index);
+    return index >= 0 ? index : 0;
+}
