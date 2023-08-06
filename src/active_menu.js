122ca879596b2e8db950350d9e248226ead2afbe
hongwr <hongwr88@gmail.com>
2023-08-06 22:24:23 +0900

    update Observer

--- src/active_menu.js
+++ src/active_menu.js
@@ -4,6 +4,7 @@ const sectionIds = ['#home', '#about', '#skills', '#work', '#contact'];
 const sections = sectionIds.map((id) => document.querySelector(id));
 const navItems = sectionIds.map((id) => document.querySelector(`[href="${id}"]`));
 const visibleSections = sectionIds.map(() => false);
+let activeNavItem = navItems[0];
 
 const options = {
     rootMargin: '-20% 0px 0px 0px',
@@ -17,17 +18,22 @@ function observerCallback(entries) {
     entries.forEach((entry) => {
         const index = sectionIds.indexOf(`#${entry.target.id}`);
         visibleSections[index] = entry.isIntersecting;
-        selectLastOne = index === sectionIds.length - 1 && entry.isIntersecting && entry.intersectionRatio >= 0.98;
+        selectLastOne = index === sectionIds.length - 1 && entry.isIntersecting && entry.intersectionRatio >= 0.95;
     });
-    console.log(visibleSections);
-    console.log('무조건 라스트 섹션!!', selectLastOne);
 
     const navIndex = selectLastOne ? sectionIds.length - 1 : findFirstIntersecting(visibleSections);
-    console.log(sectionIds[navIndex]);
+    selectNavItem(navIndex);
 }
 
 function findFirstIntersecting(intersections) {
     const index = intersections.indexOf(true);
-    console.log('첫번째 인덱스', index);
     return index >= 0 ? index : 0;
+}
+
+function selectNavItem(index) {
+    const navItem = navItems[index];
+    if (!navItem) return;
+    activeNavItem.classList.remove('active');
+    activeNavItem = navItem;
+    activeNavItem.classList.add('active');
 }
